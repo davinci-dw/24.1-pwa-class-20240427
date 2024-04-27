63160c0c73b8c859812dedb66ab2040aa664b2bf
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-27 17:32:54 -0300

    feat: consultar datos de json usando fetch

--- scrips.js
+++ scrips.js
@@ -1,1 +1,7 @@
-console.log("anda")
+const URL = "http://localhost:3000/data.json";
+
+fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log("data:", data.datos)
+    });
