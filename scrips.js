fcad6d9c0c2d94036da8efaadd8ff7b8a1d5ba1b
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-27 18:12:30 -0300

    feat: mostrar datos obtenidos

--- scrips.js
+++ scrips.js
@@ -1,16 +1,31 @@
 const URL = "http://localhost:3000/data.json";
 
+const mostrarTarjeta = (infusion) => {
+    const lista = document.querySelector('.lista');
+    const tarjeta = document.createElement('div');
+    tarjeta.className="tarjeta"; 
+    tarjeta.innerText = JSON.stringify(infusion)
+    lista.appendChild(tarjeta);
+} 
+
+const mostrarDatos = (datos) => {
+    datos.forEach(dato => {
+        mostrarTarjeta(dato);
+    })
+} 
+
 const consultarDatos = async () => {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data)
     const datosOrdenados = data.datos;
     datosOrdenados.sort((a,b)=> {
         if(a.id > b.id) return 1;
         if(a.id < b.id) return -1;
         return 0;
     })
-    console.log("data modificada: ", datosOrdenados)
+    return datosOrdenados;
 }
 
-consultarDatos();
+consultarDatos().then(datos => {
+    mostrarDatos(datos);
+})
