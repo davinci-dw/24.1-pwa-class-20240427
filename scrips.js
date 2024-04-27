c4bdf7a50b4680645e0f0434636570059a54235f
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-27 17:43:59 -0300

    feat: ordenar datos por id

--- scrips.js
+++ scrips.js
@@ -3,5 +3,14 @@ const URL = "http://localhost:3000/data.json";
 fetch(URL)
     .then(response => response.json())
     .then(data => {
-        console.log("data:", data.datos)
-    });
+        const datosOrdenados = data.datos;
+        datosOrdenados.sort((a,b)=> {
+            if(a.id > b.id) return 1;
+            if(a.id < b.id) return -1;
+            return 0;
+        })
+        return datosOrdenados;
+    })
+    .then(data => {
+        console.log("data modificada: ", data)
+    })
