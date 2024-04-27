ff5924a348f19a9a482f04766ed0eac756f504b5
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-27 18:02:36 -0300

    refactor: reemplazar promise con async/await

--- scrips.js
+++ scrips.js
@@ -1,16 +1,16 @@
 const URL = "http://localhost:3000/data.json";
 
-fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        const datosOrdenados = data.datos;
-        datosOrdenados.sort((a,b)=> {
-            if(a.id > b.id) return 1;
-            if(a.id < b.id) return -1;
-            return 0;
-        })
-        return datosOrdenados;
+const consultarDatos = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data)
+    const datosOrdenados = data.datos;
+    datosOrdenados.sort((a,b)=> {
+        if(a.id > b.id) return 1;
+        if(a.id < b.id) return -1;
+        return 0;
     })
-    .then(data => {
-        console.log("data modificada: ", data)
-    })
+    console.log("data modificada: ", datosOrdenados)
+}
+
+consultarDatos();
